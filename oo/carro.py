346b0c47dd7b339adfbc268491a9d1d510417bdf
CyroGund <cyro.gondim@gmail.com>
2022-02-07 23:38:21 -0300

    Explicacao do exercicio de orientação a objeto

--- oo/carro.py
+++ oo/carro.py
@@ -16,5 +16,83 @@ A direção tera a responsabilidade de controlar a rota.Ela oferece os seguintes
 2) Metodo girar_a_direita
 3) Metodo girar_a_esqueda
 
+  N
+O   L
+  S
 
+
+    Exemplo:
+    # Testando Motor
+    >>> motor = Motor()
+    >>> motor.velocidade
+    0
+    >>> motor.acelerar()
+    >>> motor.velocidade
+    1
+    >>> motor.acelerar()
+    >>> motor.velocidade
+    2
+    >>> motor.acelerar()
+    >>> motor.velocidade
+    3
+    >>> motor.frear()
+    >>> motor.velocidade
+    1
+    >>> motor.frear()
+    >>> motor.velocidade
+    0
+    >>> #Testando a direção
+    >>> direcao = Direcao()
+    >>> direcao.valor
+    'Norte'
+    >>> direcao.girar_a_direita()
+    >>> direcao.valor
+    'Leste'
+    >>> direcao.girar_a_direita()
+    >>> direcao.valor
+    'Leste'
+    >>> direcao.girar_a_direita()
+    >>> direcao.valor
+    'Sul'
+    >>> direcao.girar_a_direita()
+    >>> direcao.valor
+    'Oeste'
+    >>> direcao.girar_a_direita()
+    >>> direcao.valor
+    'Norte'
+    >>> direcao.girar_a_esquerda()
+    >>> direcao.valor
+    'Oeste'
+    >>> direcao.girar_a_esquerda()
+    >>> direcao.valor
+    'Sul'
+    >>> direcao.girar_a_esquerda()
+    >>> direcao.valor
+    'Leste'
+    >>> direcao.girar_a_esquerda()
+    >>> direcao.valor
+    'Norte'
+    >>> carro = Carro(direcao, motor)
+    >>> carro.calcular_velocidade()
+    0
+    >>> carro.acelerar()
+    >>> carro.calcular_velocidade()
+    1
+    >>> carro.acelerar()
+    >>> carro.calcular_velocidade()
+    2
+    >>> carro.frear()
+    >>> carro.calcular_velocidade()
+    0
+    >>> carro.calcular_direcao()
+    >>> 'Norte'
+    >>> carro.girar_a_direita()
+    >>> carro.calcular_direcao()
+    >>> 'Leste'
+    >>> carro.girar_a_esquerda()
+    >>> carro.calcular_direcao()
+    >>> 'Norte'
+    >>> carro.girar_a_esquerda()
+    >>> carro.calcular_direcao()
+    >>> 'Oeste'
 """
