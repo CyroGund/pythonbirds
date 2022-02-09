dbec970d868bd5b8657cace2f41a6253659fad17
CyroGund <cyro.gondim@gmail.com>
2022-02-09 00:14:51 -0300

    Implementando a classe direção.

--- oo/carro.py
+++ oo/carro.py
@@ -119,25 +119,30 @@ class Carro:
         self.direcao.girar_a_esquerda()
 
 
-NORTE = 'NORTE'
+NORTE = 'Norte'
 SUL = 'Sul'
 LESTE = 'Leste'
 OESTE = 'Oeste'
 
+
 class Direcao:
+
     rotacao_a_direita_dct = {
-        NORTE: LESTE, LESTE:SUL, SUL: OESTE, OESTE: NORTE
+        NORTE: LESTE, LESTE: SUL, SUL: OESTE, OESTE: NORTE
     }
-    rotacao_a_esquerda_dct ={
+    rotacao_a_esquerda_dct = {
         NORTE: OESTE, OESTE: SUL, SUL: LESTE, LESTE: NORTE
     }
+
     def __init__(self):
         self.valor = NORTE
 
     def girar_a_direita(self):
-        self.valor =self.rotacao_a_direita_dct[self.valor]
+        self.valor = self.rotacao_a_direita_dct[self.valor]
+
     def girar_a_esquerda(self):
-        self.valor=self.rotacao_a_esquerda_dct[self.valor]
+        self.valor = self.rotacao_a_esquerda_dct[self.valor]
+
 
 class Motor:
     def __init__(self):
@@ -149,10 +154,3 @@ class Motor:
     def frear(self):
         self.velocidade -= 2
         self.velocidade = max(0, self.velocidade)
-
-
-
-
-
-
-
